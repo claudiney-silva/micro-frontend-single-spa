6b1f72549d5214eea06322b613bcadd8e0dd43be
Claudiney Calixto da Silva <clau.li.erd@gmail.com>
2023-03-06 08:47:39 -0300

    chore: Utility module

--- react-parcel/src/App.js
+++ react-parcel/src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
+import { listenEvent } from '@ccs/utils'
 
 const App = ({ name }) => {
     const [tasks, updateTasks] = useState([])
 
     useEffect(() => {
-        window.addEventListener('@ccs/react-route/todo/add-task', event => {
+        listenEvent('@ccs/react-route/todo/add-task', event => {
             updateTasks(oldTasks => [
                 ...oldTasks,
                 event.detail,
